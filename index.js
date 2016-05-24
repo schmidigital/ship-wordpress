70265476ed9a2a0e95e07e91eee9741a030fc1a6
smith64fx <michael@schmidigital.de>
2016-05-24 23:59:33 +0200

    update

--- index.js
+++ index.js
@@ -215,7 +215,10 @@ module.exports = function (ship) {
         fs.writeFileSync(docker_compose_dest, docker_compose_file); 
 
         
-        utils.shell("docker-compose up -d")
+        syncExec("docker-compose up -d")
+        
+        process.chdir('./www/angular');
+        utils.shell("npm start")
     }
 
 
